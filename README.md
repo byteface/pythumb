6447d69e030dea525ae5b94e880054bce9421465
byteface <byteface@googlemail.com>
2015-04-28 08:21:52 +1000

    fix up

--- README.md
+++ README.md
@@ -3,34 +3,40 @@
 Simple test. pass it images, returns base64 strings to be used for things like thumbnails
 
 Currently just pass 2 params. http path to image and a width contraint.
-
-'''
+```
 http://localhost:5000/thumbnail?image=http://127.0.0.1:5000/static/img/landscape.png?w=100
-'''
-
+```
 would return something like
-
-'''
+```
 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADQAAABLCAIAAACwbjOoAAAd4UlEQVR4nE16Waxm2XXWt9ba+0z/f+d...
-'''
+```
 
-For a working example see the index.html file.
+For a working example see the index.html file in the templates folder.
 
-and that's almost as much as I need it for at the mo, Saves having to create/manage thumnail images on the server as I can just create them on request.
+and that's almost as much as I need it for at the mo, Saves having to create/manage thumbnail images on the server as I can just create them on request.
 
 ##RUN
 
 to run the example create a virutal environment and install the requirements, then run
-
-'''
+```
 python server.py
-'''
+```
 
 img folder contains a mixture of PNGS and JPG files to play with but only really tested the PNGS
 
 ##NOTE
 I had to use threaded=True parameter in the flask app or it would just lock up when making the image request. I think it only happens when loading images from localhost? I think that port was busy serving my app, either that or something to do with proxies? anyways I had issues with the 'requests' library and urllib and urllib2. Spent ages trying to figure out why but that resovled it. see at the bottom of the app...
-
-'''
+```
 app.run(threaded=True)
-'''
+```
+
+##YOU WILL NEED TO CONFIG STUFF DEPENDING ON PLATFORM
+http://stackoverflow.com/questions/8915296/python-image-library-fails-with-message-decoder-jpeg-not-available-pil
+
+
+##lists
+if you like flask checkout the list renderer
+
+http://localhost:5000/list
+
+and the list.html template for implementation
--- server.py
+++ server.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request, render_template, jsonify, Response, session
-import json
+from flask import Flask, request, render_template#, jsonify, Response, session
+#import json
 
 from PIL import Image
 
 import urllib2 as urllib
-import io
-
 #import urllib
+#import io
 import cStringIO
 from StringIO import StringIO
 import base64
@@ -17,19 +16,41 @@ import base64
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-# simple app return base64 thumbnail strings
-# so html can render them on they fly
 
-# NOTE - YOU WILL NEED TO CONFIG SHIT DEPENDING ON PLATFORM
-# http://stackoverflow.com/questions/8915296/python-image-library-fails-with-message-decoder-jpeg-not-available-pil
+# router ----
 
 @app.route('/')
 def index():
 	return render_template( "index.html" )
 
-@app.route('/thumbnail', methods=['GET','POST'])
-def thumbmail():
 
+@app.route('/list')
+def list():
+	"""
+	test rendering a list in a flask template
+	"""
+	somelist=[ { 'link':'http://127.0.0.1:5000/static/img/landscape.png' },\
+	{ 'link':'http://127.0.0.1:5000/static/img/portrait.JPG' },\
+	{ 'link':'http://127.0.0.1:5000/static/img/square.png' },\
+	{ 'link':'http://127.0.0.1:5000/static/img/test.png' },\
+	{ 'link':'http://127.0.0.1:5000/static/img/transparent.png' }]
+
+	return render_template( "list.html", list=somelist )
+
+
+@app.route('/thumbnail', methods=['GET','POST'])
+def thumbnail():
+	return generate_thumbnail( request.args['image'], float( request.args['w'] ) );
+
+
+
+# functions ----
+
+def generate_thumbnail( url, width ):
+	"""
+	url - a http url
+	width - cast as float if grabbing off a request arg
+	"""
 	try:
 
 		# - opens a file. won't work for a url.
@@ -46,7 +67,7 @@ def thumbmail():
 		#print image.size
 
 
-		myfile = urllib.urlopen(request.args['image'])
+		myfile = urllib.urlopen(url)
 		image = Image.open( StringIO( myfile.read()) )
 
 
@@ -54,17 +75,17 @@ def thumbmail():
 		 	image.convert("RGB")
 
 
-		if request.args['w']:
-			w = float(request.args['w'])
+		if width:
+			w = float(width)
 			#ratio=1
 			if image.size[0] > w:
 				ratio=image.size[0]/w
 
-			newWidth = image.size[0]/ratio
-			newHeight = image.size[1]/ratio
+				newWidth = image.size[0]/ratio
+				newHeight = image.size[1]/ratio
 
-			#image = image.resize( [200,100], 1 )
-			image.thumbnail( (newWidth,newHeight), Image.ANTIALIAS )
+				#image = image.resize( [200,100], 1 )
+				image.thumbnail( (newWidth,newHeight), Image.ANTIALIAS )
 
 
 		img_buffer = cStringIO.StringIO()
@@ -81,7 +102,17 @@ def thumbmail():
 	return "FAIL!"
 
 
-# INIT APPLICATION ------------------------------------------>
+
+# context processor to make function available in templates
+@app.context_processor
+def thumbnail_processor():
+	def getThumbnail( path, width ):
+		return generate_thumbnail( path, width )
+	return dict(getThumbnail=getThumbnail)
+
+
+
+# INIT APPLICATION ----
 if __name__=="__main__":
     #app.run(host='0.0.0.0', port=8080)
     app.run(threaded=True)
